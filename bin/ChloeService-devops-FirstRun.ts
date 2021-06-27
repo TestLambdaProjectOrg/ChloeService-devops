ccc4824cadc49c55760669a61ce380ebd267d43b
Can Nahum <cannahum@outlook.com>
2021-06-26 23:41:00 -0700

    fixed first run

--- bin/ChloeService-devops-FirstRun.ts
+++ bin/ChloeService-devops-FirstRun.ts
@@ -2,29 +2,29 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import Environment from '../lib/Environment';
-// import ChloeServiceStack from '../lib/ChloeServiceStack';
+import ChloeServiceStack from '../lib/ChloeServiceStack';
 // import ChloeServiceCICD from '../lib/ChloeServiceCICD';
 import ChloeServiceCICD from '../lib/ChloeServiceCICD-FirstRun';
 
 const app = new cdk.App();
 
-// const ppdStack = new ChloeServiceStack(
-//   app,
-//   `${ChloeServiceStack.STACK_NAME}${Environment.PPD}`,
-//   {
-//   //   env: { region: 'us-east-1' },
-//     appEnv: Environment.PPD,
-//   },
-// );
+const ppdStack = new ChloeServiceStack(
+  app,
+  `${ChloeServiceStack.STACK_NAME}${Environment.PPD}`,
+  {
+  //   env: { region: 'us-east-1' },
+    appEnv: Environment.PPD,
+  },
+);
 
-// const prdStack = new ChloeServiceStack(
-//   app,
-//   `${ChloeServiceStack.STACK_NAME}${Environment.PRD}`,
-//   {
-//   //   env: { region: 'us-east-1' },
-//     appEnv: Environment.PRD,
-//   },
-// );
+const prdStack = new ChloeServiceStack(
+  app,
+  `${ChloeServiceStack.STACK_NAME}${Environment.PRD}`,
+  {
+  //   env: { region: 'us-east-1' },
+    appEnv: Environment.PRD,
+  },
+);
 
 // eslint-disable-next-line no-new
 new ChloeServiceCICD(
